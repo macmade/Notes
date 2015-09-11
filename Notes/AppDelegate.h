7c3135cdb5521f785d581534ad178ee425999d8a
macmade <macmade@xs-labs.com>
2015-09-11 19:49:07 +0200

    Empty CoreData project.

---
+++ Notes/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Notes
+//
+//  Created by Macmade on 11.09.2015.
+//  Copyright © 2015 XS-Labs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ Notes/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Notes
+//
+//  Created by Macmade on 11.09.2015.
+//  Copyright © 2015 XS-Labs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
